93794068f393053cea9584fb6713ada552f0f785
Alex Khilko <alex.khilko@tripledotstudios.com>
2024-05-14 05:57:49 +0200

    Add support of option to count characters

--- wc.go
+++ wc.go
@@ -14,38 +14,40 @@ var (
 	cFlag bool
 	lFlag bool
 	wFlag bool
+	mFlag bool
 )
 
-func countFile(r io.Reader, countLines, countWords bool) (int, int, int, error) {
-	buf := make([]byte, 4096)
-	bytesCount := 0
+func getFileCounts(file *os.File, countLines, countWords bool) (int, int, int, int, error) {
 	const lineSep = '\n'
+	bytesCount := 0
 	linesCount := 0
 	wordCount := 0
+	charCount := 0
 	curWord := false
+
+	reader := bufio.NewReader(file)
 	for {
-		n, err := r.Read(buf)
-		bytesCount += n
-		for _, b := range(buf[:n]) {
-			if countLines && b == lineSep {
-				linesCount += 1
-			}
-			if countWords {
-				if unicode.IsSpace(rune(b)) {
-					curWord = false
-				} else {
-					if !curWord {
-						wordCount++
-					}
-					curWord = true
-				}
-			}
-		}
+		r, size, err := reader.ReadRune()
 		if err == io.EOF {
-			return bytesCount, linesCount, wordCount, nil 
+			return bytesCount, linesCount, wordCount, charCount, nil 
 		}
 		if err != nil {
-			return bytesCount, linesCount, wordCount, err
+			return bytesCount, linesCount, wordCount, charCount, err
+		}
+		charCount++
+		bytesCount += size
+		if countLines && r == lineSep {
+			linesCount += 1
+		}
+		if countWords {
+			if unicode.IsSpace(r) {
+				curWord = false
+			} else {
+				if !curWord {
+					wordCount++
+				}
+				curWord = true
+			}
 		}
 	}
 }
@@ -54,7 +56,14 @@ func main() {
 	flag.BoolVar(&cFlag, "c", false, "Count the number of bytes in file")
 	flag.BoolVar(&lFlag, "l", false, "Count the number of lines in file")
 	flag.BoolVar(&wFlag, "w", false, "Count the number of words in file")
+	flag.BoolVar(&mFlag, "m", false, "Count the number of characters in file")
 	flag.Parse()
+	if !cFlag && !lFlag && !wFlag && !mFlag {
+		cFlag = true
+		lFlag = true
+		wFlag = true
+		mFlag = true
+	}
 	arguments := flag.Args()
 	if len(arguments) != 1 {
 		panic("invalid number of arguments")
@@ -65,8 +74,7 @@ func main() {
 		panic("failed to open a file")
 	}
 	defer f.Close()
-	reader := bufio.NewReader(f)
-	bytes, lines, words, err := countFile(reader, lFlag, wFlag)
+	bytes, lines, words, chars, err := getFileCounts(f, lFlag, wFlag)
 	if err != nil {
 		panic("failed to count number of bytes")
 	}
@@ -79,6 +87,9 @@ func main() {
 	}
 	if wFlag {
 		result += " " + strconv.Itoa(words)
+	}
+	if mFlag {
+		result += " " + strconv.Itoa(chars)
 	}
 	fmt.Println("", result, filename)
 }
